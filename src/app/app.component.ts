5f08c3362ecf8f67b11bfca9709b55ba051d07bc
João Victor Olivo <jvso@yahoo.com>
2024-09-24 11:18:06 -0300

    Service typing with interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { UserService } from './shared/services/user.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,12 @@ import { RouterOutlet } from '@angular/router';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'TrainSys';
+
+  constructor(private userService: UserService) {}
+
+  ngOnInit(): void {
+    this.userService.get('7417').subscribe(data => console.log(data));
+  }
 }
--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,32 +1,33 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { UserInterface } from '../interfaces/user.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-  url = 'http://localhost:3000/users';
+  url = 'http://localhost:3000/user';
 
   constructor(private httpClient: HttpClient) { }
 
-  getAll() {
-    return this.httpClient.get(this.url);
+  getAll(): Observable<UserInterface[]> {
+    return this.httpClient.get<UserInterface[]>(this.url);
   }
 
-  get(id: number) {
-    return this.httpClient.get(`${this.url}/${id}`);
+  get(id: string): Observable<UserInterface> {
+    return this.httpClient.get<UserInterface>(`${this.url}/${id}`);
   }
 
-  create(user: UserInterface) {
+  create(user: UserInterface): Observable<Object> {
     return this.httpClient.post(this.url, user);
   }
 
-  update(id: number, user: UserInterface) {
+  update(id: number, user: UserInterface): Observable<Object> {
     return this.httpClient.put(`${this.url}/${id}`, user);
   }
 
-  delete(id: number) {
+  delete(id: number): Observable<Object> {
     return this.httpClient.delete(`${this.url}/${id}`);
   }
 }
