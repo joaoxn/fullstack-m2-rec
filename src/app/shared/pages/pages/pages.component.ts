6a9b014b39855e9b745878c1c741386ae3fdda38
João Victor Olivo <jvso@yahoo.com>
2024-09-29 19:22:42 -0300

    Header and Footer releasal

--- src/app/shared/pages/pages/pages.component.ts
+++ src/app/shared/pages/pages/pages.component.ts
@@ -1,23 +1,35 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Router, RouterLink, RouterOutlet } from '@angular/router';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatTabsModule} from '@angular/material/tabs';
-import { UserInterface } from '../../interfaces/user.interface';
-import { UserService } from '../../services/user.service';
+import { NgStyle } from '@angular/common';
 
 
 @Component({
   selector: 'app-pages',
   standalone: true,
-  imports: [RouterOutlet, RouterLink, MatTabsModule, MatToolbarModule, MatButtonModule, MatIconModule],
+  imports: [RouterOutlet , NgStyle, RouterLink, MatTabsModule, MatToolbarModule, MatButtonModule, MatIconModule],
   templateUrl: './pages.component.html',
   styleUrl: './pages.component.scss'
 })
-export class PagesComponent {
+export class PagesComponent implements AfterViewInit {
+  @ViewChild('header') headerElement!: ElementRef;
+  @ViewChild('footer') footerElement!: ElementRef;
+  
+  routerOutletMinHeight!: string;
 
-  constructor(private router: Router) { }
+  constructor(private router: Router, private cdRef: ChangeDetectorRef) { }
+  
+  ngAfterViewInit(): void {
+    const headerHeight = this.headerElement.nativeElement.offsetHeight;
+    const footerHeight = this.footerElement.nativeElement.offsetHeight;
+  
+    this.routerOutletMinHeight = `calc(100vh - ${headerHeight}px - ${footerHeight}px)`;
+
+    this.cdRef.detectChanges();
+  }
 
   logout() {
     localStorage.removeItem('authToken');
