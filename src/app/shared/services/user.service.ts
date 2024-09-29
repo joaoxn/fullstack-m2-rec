bc374bca63b93952ee73445460567709300e35cf
João Victor Olivo <jvso@yahoo.com>
2024-09-29 00:36:07 -0300

    User service registering + refactor

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -33,6 +33,8 @@ export class UserService {
   getByEmail(email: string): Observable<UserInterface | undefined> {
     return this.httpClient.get<UserInterface[]>(`${this.url}?email=${email}`).pipe(
       map(users => {
+        if (users.length == 0)
+          throw new Error("No user found with email: " + email);
         if (users.length > 1)
           console.warn(`Multiple users found with the same email (${email})`, users);
         return users[0]; // Returns undefined if the array is empty
@@ -118,37 +120,62 @@ export class UserService {
     );
   }
 
-  login(email: string, password: string): Observable<string | false> {
+  register(user: UserDtoInterface): Observable<string | undefined> {
+    return this.getByEmail(user.email).pipe(
+      map(data => {
+        console.log(data);
+        console.error("Register failed: Email already registered");
+        return undefined;
+      }),
+      catchError(() =>
+        this.add(user).pipe(
+          switchMap(user => {
+            console.info("Registered and logged in successfully");
+            return this.logSession(user.id);
+          })
+        )
+      )
+    )
+  }
+
+  login(email: string, password: string): Observable<string | undefined> {
     return this.getByEmail(email).pipe(
       map(user => {
-        if (!user) {
-          console.warn("Login unauthorized.", "Returned:", user);
-          return false;
+        if (!user || user.password != password) {
+          console.warn("Login unauthorized");
+          return undefined;
         }
-        
-        if (user.password != password) {
-          console.warn("Login verification unauthorized. Password does not match (received: " + password + ")");
-          return false;
-        }
-
-        localStorage.setItem('loginUser', JSON.stringify({
-          userId: user.id,
-          auth: Date.now() * 1000
-        }));
-
-        console.info(`Logged-in with id ${user.id} successfully`);
-        return user.id;
+        return this.logSession(user.id);
       }));
   }
 
-  validateAuth(): string | false {
+  logSession(id: string): string {
+    sessionStorage.setItem('loginUser', JSON.stringify({
+      userId: id,
+      auth: Date.now() * 1000
+    }));
+
+    console.info(`Logged-in with id ${id} successfully`);
+    return id;
+  }
+
+  validateAuth(): Observable<string> | false {
     const loginUser = localStorage.getItem('loginUser');
     if (!loginUser) return false;
 
     const auth = JSON.parse(loginUser).auth;
     if (!auth) return false;
 
-    if (Date.now() * 1000 - auth > 3600) return false; // If auth older than 3600 seconds (1 hour) returns false
-    return auth.userId;
+    const userId: string = JSON.parse(loginUser).userId;
+    if (!userId) return false;
+
+    return this.get(userId).pipe(
+      map(() => {
+        if (Date.now() * 1000 - auth < 3600) // If auth older than 3600 seconds (1 hour) returns false
+          return auth.userId;
+        console.warn("Authentication expired. Auth: "+ auth);
+        throw new Error("Authentication expired. Auth: "+ auth);
+      })
+    );
   }
 }
