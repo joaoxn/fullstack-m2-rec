4bca03468c5ee9f4a52864afabf3b7ae674ce070
João Victor Olivo <jvso@yahoo.com>
2024-10-04 11:29:14 -0300

    Created components and defined routes

---
+++ src/app/pages/student-trainings/student-trainings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-student-trainings',
+  standalone: true,
+  imports: [],
+  templateUrl: './student-trainings.component.html',
+  styleUrl: './student-trainings.component.scss'
+})
+export class StudentTrainingsComponent {
+
+}
