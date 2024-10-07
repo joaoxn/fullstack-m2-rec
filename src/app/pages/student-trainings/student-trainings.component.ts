8ab4cd0c08c8f1942224595038376e55735b15bf
João Victor Olivo <jvso@yahoo.com>
2024-10-06 23:08:58 -0300

    Created trainings visualizer page

--- src/app/pages/student-trainings/student-trainings.component.ts
+++ src/app/pages/student-trainings/student-trainings.component.ts
@@ -1,12 +1,84 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { ActivatedRoute, Router, RouterLink } from '@angular/router';
+import { MatInputModule } from '@angular/material/input';
+import { UserService } from '../../shared/services/user.service';
+import { Student } from '../../shared/interfaces/student';
+import { StudentService } from '../../shared/services/student.service';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { FormsModule } from '@angular/forms';
+import { MatTabsModule } from '@angular/material/tabs';
+import { Training } from '../../shared/interfaces/training';
+import { TrainingService } from '../../shared/services/training.service';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { NamePipe } from '../../shared/pipes/name.pipe';
+import { finalize } from 'rxjs';
 
 @Component({
   selector: 'app-student-trainings',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, FormsModule, NamePipe,
+    MatIconModule, MatInputModule, MatButtonModule,
+    MatTabsModule, MatProgressBarModule, MatCardModule, MatChipsModule],
   templateUrl: './student-trainings.component.html',
   styleUrl: './student-trainings.component.scss'
 })
 export class StudentTrainingsComponent {
+  studentId!: string;
+  student?: Student;
+  rawTrainings: Training[] = [];
+  trainings: Training[][] = [[], [], [], [], [], [], []];
 
+  isLoading = false;
+
+  constructor(
+    private trainingService: TrainingService,
+    private studentService: StudentService,
+    private userService: UserService,
+    private router: Router,
+    private activatedRoute: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    this.isLoading = true;
+    this.studentId = this.activatedRoute.snapshot.params['id'];
+    if (!this.studentId) this.router.navigate(['/students']);
+    this.getTrainings();
+  }
+
+  getTrainings() {
+    this.studentService.get(this.studentId).pipe(
+      finalize(() => this.isLoading = false)
+    ).subscribe(student => {
+      this.student = student;
+      this.trainingService.getAllByStudent(student).subscribe({
+        next: trainings => {
+          this.rawTrainings = trainings;
+          if (!trainings) return;
+
+          trainings.forEach(training => this.trainings[training.weekDay].push(training));
+        },
+        complete: () => this.isLoading = false
+      })
+    })
+  }
+
+  edit(trainingId: string) {
+    this.router.navigate(['/trainings', trainingId]);
+  }
+
+  remove(trainingId: string) {
+    this.rawTrainings = [];
+    this.trainings = [[], [], [], [], [], [], []];
+    this.isLoading = true;
+    this.studentService.unregisterTraining(this.studentId, trainingId).subscribe({
+      next: () => this.getTrainings(),
+      error: error => {
+        console.error(error);
+        this.isLoading = false;
+      }
+    });
+  }
 }
